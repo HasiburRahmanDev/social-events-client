49cfeecb3d0d404666fc39ea1e0187e669d41636
Md Hasibur Rahman <hasib2920@hotmail.com>
2025-11-10 22:19:28 +0600

    passsword show and showOff set

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,34 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../firebase.init";
+import { toast } from "react-toastify";
+import { FaEye } from "react-icons/fa";
+import { IoEyeOff } from "react-icons/io5";
 
 const Register = () => {
+  const [show, setShow] = useState(false);
   const handleRegister = (e) => {
     e.preventDefault();
 
     // const name = e.target.name.value;
     const email = e.target.email.value;
     const password = e.target.password.value;
+    const regex = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
 
-    createUserWithEmailAndPassword(auth, email, password).then().catch();
+    if (!regex.test(password)) {
+      toast.error(
+        "Password must be at least 6 characters and include both uppercase and lowercase letters."
+      );
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        toast.success("Register successfully");
+      })
+      .catch((e) => {
+        toast.error(e.message);
+      });
   };
   return (
     <div className="hero bg-base-200 min-h-screen">
@@ -26,7 +44,7 @@ const Register = () => {
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <form onSubmit={handleRegister}>
-              <fieldset className="fieldset">
+              <fieldset className="fieldset relative">
                 <label className="label">name</label>
                 <input
                   name="name"
@@ -51,10 +69,16 @@ const Register = () => {
                 <label className="label">Password</label>
                 <input
                   name="password"
-                  type="password"
+                  type={show ? "text" : "password"}
                   className="input"
                   placeholder="Password"
                 />
+                <span
+                  onClick={() => setShow(!show)}
+                  className="absolute right-[24px] top-[250px] cursor-pointer z-10"
+                >
+                  {show ? <FaEye /> : <IoEyeOff />}
+                </span>
                 <div>
                   <a className="link link-hover">Have an account?</a>
                 </div>
